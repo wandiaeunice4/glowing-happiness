01c25bdefd7f597c6ec0d6ac993e78809ddd28d9
DFirst App <wandiastan@gmail.com>
2026-09-03 13:07:38 +0300

    Revert "Lay the transactions out the way Deriv's own run panel does"
    This reverts commit dfe3babbade2d6504237e005df07ee793abe2b7f.

--- analysis/app.js
+++ analysis/app.js
@@ -613,7 +613,6 @@
     var t = C.TYPES[r.type];
     txn.add({
       label: t.label + (t.barrier ? " " + r.barrier : ""),
-      type: r.type,
       market: r.market,
       win: r.win,
       stake: r.stake,
